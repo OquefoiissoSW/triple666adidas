970aeadafd07c33ce8800f10585db9dfd18632d2
Evgeniy_Litvinov <ELoquefoiisso@gmail.com>
2025-10-05 16:17:09 +0600

    чуть переделал

--- internal/input/input.go
+++ internal/input/input.go
@@ -1,94 +1,106 @@
 package input
 
 import (
-  rl "github.com/gen2brain/raylib-go/raylib"
+	rl "github.com/gen2brain/raylib-go/raylib"
 )
 
 type State struct {
-  MoveX, MoveY        float32
-  MoveLeft, MoveRight bool
-  MoveUp, MoveDown    bool
-  Shoot, Hook, Pause  bool
-  Confirm, Cancel     bool
+	MoveX, MoveY        float32
+	MoveLeft, MoveRight bool
+	MoveUp, MoveDown    bool
+	Shoot, Hook, Pause  bool
+	Confirm, Cancel     bool
+	Fullscreen, Ulta    bool
 }
 
 var Current State
 
+// Используем константы rl для геймпада
 const (
-  gpA            = 0
-  gpB            = 1
-  gpX            = 2
-  gpRightTrigger = 7
-  gpStart        = 8
-  gpAxisLeftX    = rl.GamepadAxisLeftX
-  gpAxisLeftY    = rl.GamepadAxisLeftY
+	gpA            = rl.GamepadButtonLeftFaceDown  // A
+	gpB            = rl.GamepadButtonLeftFaceRight // B
+	gpX            = rl.GamepadButtonLeftFaceLeft  // X
+	gpY            = rl.GamepadButtonRightFaceUp   // Y
+	gpRightTrigger = rl.GamepadButtonRightTrigger1 // R1
+	gpLeftTrigger  = rl.GamepadButtonLeftTrigger1  // L1
+	gpStart        = 15                            // Start
+
+	gpAxisLeftX = rl.GamepadAxisLeftX
+	gpAxisLeftY = rl.GamepadAxisLeftY
 )
 
 func Init() {
-  // Пока не требуется дополнительная инициализация
+	// Пока не требуется дополнительная инициализация
 }
 
 func Update() {
-  gamepad := rl.IsGamepadAvailable(0)
+	gamepad := rl.IsGamepadAvailable(0)
 
-  // Сброс состояния движения
-  Current.MoveLeft = false
-  Current.MoveRight = false
-  Current.MoveUp = false
-  Current.MoveDown = false
-  Current.MoveX = 0
-  Current.MoveY = 0
+	// Сброс состояния движения
+	Current.MoveLeft = false
+	Current.MoveRight = false
+	Current.MoveUp = false
+	Current.MoveDown = false
+	Current.MoveX = 0
+	Current.MoveY = 0
+	Current.Shoot = false
+	Current.Hook = false
+	Current.Pause = false
+	Current.Confirm = false
+	Current.Cancel = false
+	Current.Fullscreen = false
+	Current.Ulta = false
 
-  if gamepad {
-    // Аналоговые оси движения с геймпада
-    Current.MoveX = rl.GetGamepadAxisMovement(0, gpAxisLeftX)
-    Current.MoveY = rl.GetGamepadAxisMovement(0, gpAxisLeftY)
+	if gamepad {
+		// Аналоговые оси движения с геймпада
+		Current.MoveX = rl.GetGamepadAxisMovement(0, gpAxisLeftX)
+		Current.MoveY = rl.GetGamepadAxisMovement(0, gpAxisLeftY)
 
-    // Булевы направления с геймпада (если осей мало или нет)
-    if Current.MoveX < -0.5 {
-      Current.MoveLeft = true
-    }
-    if Current.MoveX > 0.5 {
-      Current.MoveRight = true
-    }
-    if Current.MoveY < -0.5 {
-      Current.MoveUp = true
-    }
-    if Current.MoveY > 0.5 {
-      Current.MoveDown = true
-    }
+		// Булевы направления с геймпада
+		if Current.MoveX < -0.5 {
+			Current.MoveLeft = true
+		}
+		if Current.MoveX > 0.5 {
+			Current.MoveRight = true
+		}
+		if Current.MoveY < -0.5 {
+			Current.MoveUp = true
+		}
+		if Current.MoveY > 0.5 {
+			Current.MoveDown = true
+		}
 
-    // Действия с геймпада
-    Current.Shoot = rl.IsGamepadButtonDown(0, gpRightTrigger)
-    Current.Hook = rl.IsGamepadButtonDown(0, gpX)
-    Current.Pause = rl.IsGamepadButtonDown(0, gpStart)
-    Current.Confirm = rl.IsGamepadButtonDown(0, gpA)
-    Current.Cancel = rl.IsGamepadButtonDown(0, gpB)
+		// Действия с геймпада (только при нажатии)
+		Current.Shoot = rl.IsGamepadButtonPressed(0, gpRightTrigger)
+		Current.Hook = rl.IsGamepadButtonPressed(0, gpX)
+		Current.Pause = rl.IsGamepadButtonPressed(0, gpStart)
+		Current.Confirm = rl.IsGamepadButtonPressed(0, gpA)
+		Current.Cancel = rl.IsGamepadButtonPressed(0, gpB)
+		Current.Fullscreen = rl.IsGamepadButtonPressed(0, gpLeftTrigger)
+		Current.Ulta = rl.IsGamepadButtonDown(0, rl.GamepadButtonRightFaceUp)
 
-  } else {
-    // Движение с клавиатуры
-    if rl.IsKeyDown(rl.KeyA)  rl.IsKeyDown(rl.KeyLeft) {
-      Current.MoveLeft = true
-    }
-    if rl.IsKeyDown(rl.KeyD)  rl.IsKeyDown(rl.KeyRight) {
-      Current.MoveRight = true
-    }
-    if rl.IsKeyDown(rl.KeyW)  rl.IsKeyDown(rl.KeyUp) {
-      Current.MoveUp = true
-    }
-    if rl.IsKeyDown(rl.KeyS)  rl.IsKeyDown(rl.KeyDown) {
-      Current.MoveDown = true
-    }
+	} else {
+		// Движение с клавиатуры
+		if rl.IsKeyDown(rl.KeyA) || rl.IsKeyDown(rl.KeyLeft) {
+			Current.MoveLeft = true
+		}
+		if rl.IsKeyDown(rl.KeyD) || rl.IsKeyDown(rl.KeyRight) {
+			Current.MoveRight = true
+		}
+		if rl.IsKeyDown(rl.KeyW) || rl.IsKeyDown(rl.KeyUp) {
+			Current.MoveUp = true
+		}
+		if rl.IsKeyDown(rl.KeyS) || rl.IsKeyDown(rl.KeyDown) {
+			Current.MoveDown = true
+		}
 
-    // Аналоговых осей нет для клавиатуры
-    Current.MoveX = 0
-    Current.MoveY = 0
-
-    // Действия с клавиатуры и мыши
-    Current.Shoot = rl.IsMouseButtonDown(rl.MouseButtonRight)
-    Current.Hook = rl.IsKeyDown(rl.KeyQ)
-    Current.Pause = rl.IsKeyDown(rl.KeyEscape)
-    Current.Confirm = rl.IsKeyDown(rl.KeyEnter)
-    Current.Cancel = rl.IsKeyDown(rl.KeyEscape)
-  }
+		// Действия с клавиатуры и мыши (только при нажатии)
+		Current.Shoot = rl.IsMouseButtonPressed(rl.MouseButtonRight)
+		Current.Hook = rl.IsKeyPressed(rl.KeyQ)
+		Current.Pause = rl.IsKeyPressed(rl.KeyEscape)
+		Current.Confirm = rl.IsKeyPressed(rl.KeyEnter)
+		Current.Cancel = rl.IsKeyPressed(rl.KeyEscape)
+		Current.Fullscreen = rl.IsKeyPressed(rl.KeyF11)
+		Current.Ulta = rl.IsKeyPressed(rl.KeyE)
+	}
 }
